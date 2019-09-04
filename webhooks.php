c6a56be3f7ba5e957c012074562843c71b7b4988
cetuch <53572644+cetuch@users.noreply.github.com>
2019-09-04 11:23:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'mDPBPcQ0Fr6GCiX32OuP6DwxSUh5EqJApro32CVUiOq5POInzlN+JKU+Kzp9+VxgFOhAO71muZmg80gP2Lz1mPVu8z5B8gY+txex9uRFD1Un/oWQMNAddAx+eYjUIoIdB7+vdhzBcQkkW+UfkvXJTwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
